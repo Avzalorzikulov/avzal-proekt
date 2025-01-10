d8eae55b0b597a703dc2bb0647d0c3c5e6db51e3
Gittagi ismin <Sani emaillin>
2025-01-10 18:49:29 +0500

    updated changed

--- uc-cleaned.js
+++ uc-cleaned.js
@@ -3,7 +3,4 @@ window.onerror = function(message, source, lineno, colno, error) {
         return true; // Suppress 404 errors
     }
     console.error(message, source, lineno, colno, error); // Log other errors
-    if (message.includes("404")) {
-        return true; // Suppress 404 errors
-    }
 };
